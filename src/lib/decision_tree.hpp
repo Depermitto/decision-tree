cf4f97d199823b67f55cedb19eb1b85b982946bb
Depermitto <penciller@disroot.org>
2024-12-12 22:37:10 +0100

    use std::variant in Nodes and split data on attribute

--- src/lib/decision_tree.hpp
+++ src/lib/decision_tree.hpp
@@ -1,19 +1,19 @@
 #pragma once
 #include <algorithm>
 #include <iterator>
+#include <map>
 #include <memory>
-#include <optional>
 #include <ostream>
 #include <stdexcept>
 #include <string>
-#include <string_view>
 #include <tuple>
+#include <variant>
 #include <vector>
 
 #include "rapidcsv.h"
 
 namespace uma {
-template <typename ClassType>
+template <typename Class>
 class DecisionTree {
    public:
     DecisionTree(const rapidcsv::Document &data, int max_depth = -1) {
@@ -23,7 +23,7 @@ class DecisionTree {
         const std::string class_column_label = m_attributes.back();
         m_attributes.pop_back();
 
-        std::vector<ClassType> classes = data.GetColumn<ClassType>(class_column_label);
+        std::vector<Class> classes = data.GetColumn<Class>(class_column_label);
         std::sort(classes.begin(), classes.end());
         std::unique_copy(classes.begin(), classes.end(), std::back_inserter(m_classes));
 
@@ -34,11 +34,11 @@ class DecisionTree {
         if (not m_root) throw std::runtime_error("could not fit the tree!");
     }
 
-    ClassType predict() const;
+    Class predict() const;
 
-    std::vector<ClassType> classes() const { return m_classes; }
+    std::vector<Class> classes() const { return m_classes; }
 
-    friend std::ostream &operator<<(std::ostream &os, const DecisionTree<ClassType> &decision_tree) {
+    friend std::ostream &operator<<(std::ostream &os, const DecisionTree<Class> &decision_tree) {
         os << "DecisionTree{";
         if (decision_tree.m_root) {
             os << "\n";
@@ -50,54 +50,82 @@ class DecisionTree {
 
    private:
     struct Node {
-        std::string m_attribute;
-        std::optional<double> m_threshold;
+        std::variant<Class, std::tuple<std::string, double>> m_value;
         std::unique_ptr<Node> m_left;
         std::unique_ptr<Node> m_right;
 
-        Node(const std::string &attribute, std::optional<double> threshold = std::nullopt,
+        Node(const std::variant<Class, std::tuple<std::string, double>> &value,
              std::unique_ptr<Node> left = nullptr, std::unique_ptr<Node> right = nullptr)
-            : m_attribute(attribute),
-              m_threshold(threshold),
-              m_left(std::move(left)),
-              m_right(std::move(right)) {}
+            : m_value(value), m_left(std::move(left)), m_right(std::move(right)) {}
     };
     std::unique_ptr<Node> m_root;
     rapidcsv::Document m_data;
     std::vector<std::string> m_attributes;
-    std::vector<ClassType> m_classes;
+    std::vector<Class> m_classes;
 
-    static std::unique_ptr<Node> build_tree(std::vector<std::string> &attributes, rapidcsv::Document &data,
-                                            const std::vector<ClassType> &classes, uint max_depth) {
+    static std::unique_ptr<Node> build_tree(std::vector<std::string> &attributes,
+                                            const rapidcsv::Document &data, const std::vector<Class> &classes,
+                                            uint max_depth) {
         if (attributes.size() == 0 || max_depth == 0) {
-            return std::make_unique<Node>("leaf");  // TODO: most common class from data
+            std::map<Class, uint> common;
+            for (uint row_idx = 0; row_idx < data.GetRowCount(); row_idx++) {
+                common[data.GetCell<Class>(data.GetColumnCount() - 1, row_idx)] += 1;
+            }
+
+            return std::make_unique<Node>(
+                std::max_element(common.begin(), common.end(), [](const auto &p1, const auto &p2) {
+                    return p1.second < p2.second;
+                })->first);
         }
 
         const auto [attribute, threshold] = test(attributes, data, classes);
-        // const auto [left_data, right_data] = TODO;
+        const auto [left_data, right_data] = split_data(attribute, data, threshold);
         attributes.erase(std::find(attributes.begin(), attributes.end(), attribute));
 
-        return std::make_unique<Node>(attribute, threshold,
-                                      build_tree(attributes, data, classes, max_depth - 1),
-                                      build_tree(attributes, data, classes, max_depth - 1));
+        return std::make_unique<Node>(std::make_tuple(attribute, threshold),
+                                      build_tree(attributes, left_data, classes, max_depth - 1),
+                                      build_tree(attributes, right_data, classes, max_depth - 1));
     }
 
     static std::tuple<std::string, double> test(const std::vector<std::string> &attributes,
                                                 const rapidcsv::Document &data,
-                                                const std::vector<ClassType> &classes) {
+                                                const std::vector<Class> &classes) {
+        // TODO
         const auto attr = attributes[0];
         const auto tr = data.GetCell<double>(data.GetColumnIdx(attr), 0);
         return std::make_tuple(attr, tr);
     }
 
-    static double ig(std::string_view attribute, const rapidcsv::Document &data,
-                     std::optional<double> threshold);
+    static std::tuple<rapidcsv::Document, rapidcsv::Document> split_data(const std::string &attribute,
+                                                                         const rapidcsv::Document &data,
+                                                                         double threshold) {
+        rapidcsv::Document left = data;
+        rapidcsv::Document right = data;
+        uint attribute_column_idx = data.GetColumnIdx(attribute);
+        for (uint row_idx = 0; row_idx < data.GetRowCount(); row_idx++) {
+            const auto val = data.GetCell<double>(attribute_column_idx, row_idx);
+            if (val < threshold) {
+                right.RemoveRow(row_idx);
+            } else {
+                left.RemoveRow(row_idx);
+            }
+        }
+
+        return std::make_tuple(left, right);
+    }
+
+    // static double ig(const std::string &attribute, const rapidcsv::Document &data,
+    //                  std::optional<double> threshold) {
+    // }
 
     static void stream(std::ostream &os, const std::unique_ptr<Node> &node, uint depth = 1) {
-        os << "(" << node->m_attribute;
-        if (node->m_threshold) {
-            os << ", " << *node->m_threshold;
+        if (std::holds_alternative<Class>(node->m_value)) {
+            os << "(" << std::get<Class>(node->m_value);
+        } else {
+            const auto [a, t] = std::get<std::tuple<std::string, double>>(node->m_value);
+            os << "(" << a << ", " << t;
         }
+
         os << ")\n";
         if (node->m_left) {
             os << std::setw(depth * 4 + 3) << "L: ";
